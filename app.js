060b9290c6af4a2736c501a7b9009ae34e737b08
Ayomikun Wahab-Jimoh <80535194+Mkzay@users.noreply.github.com>
2024-07-01 20:31:17 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const dayOfWeekElement = document.getElementById("dayOfWeek");
-    const utcTimeElement = document.getElementById("utcTime");
-  
-    function updateTime() {
-      const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-      const currentDate = new Date();
-      const dayOfWeek = daysOfWeek[currentDate.getUTCDay()];
-      const utcTime = currentDate.getTime();
-  
-      dayOfWeekElement.textContent = dayOfWeek;
-      utcTimeElement.textContent = utcTime;
-    }
-  
-    updateTime();
-  
-    setInterval(updateTime, 1000);
-  });
+  const dayOfWeekElement = document.getElementById("dayOfWeek");
+  const utcTimeElement = document.getElementById("utcTime");
+
+  function updateTime() {
+    const currentDate = new Date();
+    const dayOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"][currentDate.getUTCDay()];
+
+    const hours = currentDate.getUTCHours().toString().padStart(2, "0");
+    const minutes = currentDate.getUTCMinutes().toString().padStart(2, "0");
+    const seconds = currentDate.getUTCSeconds().toString().padStart(2, "0");
+    const formattedUTCTime = `${hours}:${minutes}:${seconds}`;
+
+    dayOfWeekElement.textContent = dayOfWeek;
+    utcTimeElement.textContent = formattedUTCTime;
+  }
+
+  updateTime();
+  setInterval(updateTime, 1000);
+});
